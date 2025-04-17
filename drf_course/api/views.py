1f4fab5f0228b62779fa457aaab9023e963f0a85
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-17 10:51:15 +0530

    Added Permissions and  Tests

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -5,6 +5,7 @@ from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 
 # @api_view(['GET'])
@@ -41,6 +42,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
   queryset = Order.objects.prefetch_related('items__product')
   serializer_class = OrderSerializer
+  permission_classes = [IsAuthenticated]
   
   def get_queryset(self):
     user = self.request.user
